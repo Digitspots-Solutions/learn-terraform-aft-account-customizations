04f43582c637549143e2d05a1663d72beb08ee48
Charles Ngerem <charlesngerem12@gmail.com>
2025-12-29 08:14:19 -0500

    fix: Use data source for PortalAutomationRole in sandbox stack
    The role is created by AFT account-customizations, so when the portal
    deploys the sandbox stack, we should reference it (data source) not
    create it (resource) to avoid EntityAlreadyExists errors.

--- sandbox/terraform/iam.tf
+++ sandbox/terraform/iam.tf
@@ -1,38 +1,19 @@
 # Portal Automation Role - allows portal to deploy infrastructure in this account
-# This role is created in EACH vended account by account-customizations
+# This role is CREATED by AFT account-customizations, so we just reference it here
+# to avoid "EntityAlreadyExists" errors when the portal deploys this stack
 
-resource "aws_iam_role" "portal_automation" {
+data "aws_iam_role" "portal_automation" {
   name = "PortalAutomationRole"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = [
-            "arn:aws:iam::929557547206:role/opportunity-portal-dev-lambda-role",
-            "arn:aws:iam::929557547206:role/opportunity-portal-dev-codebuild-terraform"
-          ]
-        }
-        Action = "sts:AssumeRole"
-        Condition = {
-          StringEquals = {
-            "sts:ExternalId" = "opportunity-portal-automation"
-          }
-        }
-      }
-    ]
-  })
 }
 
-# Attach AdministratorAccess for full infrastructure deployment capabilities
+# AdministratorAccess should already be attached by AFT
+# This is idempotent - Terraform will not fail if already attached
 resource "aws_iam_role_policy_attachment" "portal_automation_admin" {
-  role       = aws_iam_role.portal_automation.name
+  role       = data.aws_iam_role.portal_automation.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 output "portal_automation_role_arn" {
-  value       = aws_iam_role.portal_automation.arn
+  value       = data.aws_iam_role.portal_automation.arn
   description = "ARN of the Portal Automation Role"
 }
